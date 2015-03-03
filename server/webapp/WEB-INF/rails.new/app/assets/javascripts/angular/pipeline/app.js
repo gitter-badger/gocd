85abb176f88554bf73fadef46a98dac80c9d5676
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2015-03-03 16:13:56 +0530

    #776 - Initial styling to list the pipeline run and the stages.

--- server/webapp/WEB-INF/rails.new/app/assets/javascripts/angular/pipeline/app.js
+++ server/webapp/WEB-INF/rails.new/app/assets/javascripts/angular/pipeline/app.js
@@ -1,5 +1,45 @@
 'use strict';
 
-angular.module('pipeline', []).controller('PipelineHistoryController', ['$scope', function($scope) {
+angular.module('pipeline', []).service('PipelineService', ['$http', function ($http) {
+    return {
+        getHistory: function (pipelineName) {
+            //return the promise directly.
+            return $http.get(context_path("pipelineHistory.json?pipelineName=" + pipelineName))
+                .then(function (result) {
+                    //resolve the promise as the data
+                    return result.data;
+                });
+        }
+    }
+}]).controller('PipelineHistoryController', ['$scope', 'PipelineService', function ($scope, PipelineService) {
     $scope.heading = 'Pipeline history';
+
+    $scope.loadHistory = function (pipelineName) {
+        $scope.pipelineName = pipelineName;
+
+        PipelineService.getHistory((pipelineName)).then(function (data) {
+            $scope.data = data;
+        });
+
+    }
+
+    $scope.isPipelinePaused = function () {
+        return this.data && this.data.paused == 'true';
+    }
+
+    $scope.canPause = function () {
+        return this.data && this.data.canPause == "true";
+    }
+
+    $scope.displayForceBuildButton = function () {
+        return this.data && this.data.showForceBuildButton == 'true';
+    }
+
+    $scope.canForce = function () {
+        return this.data && this.data.canForce == 'true'
+    }
+
+    $scope.stageStatusStyle = function(status){
+        return status.toLowerCase() + "-stage"
+    }
 }]);
